dacb51bb24e7ebe51de11e31416f2311c4e9cd99
Charlie <patelchandan1212@gmail.com>
2025-03-21 14:18:32 +0545

    todos-api crud

---
+++ index.js
@@ -0,0 +1,23 @@
+
+// import { PrismaClient } from "@prisma/client";
+
+// const prisma = PrismaClient()
+
+// const main = async() => {
+//     const user = await prisma.user.create({
+//         data:{
+//             name: "Charlie",
+//             email: "charlie@example.com",
+//             mobile: "9807705388",
+//             password: "password123",
+//         }
+//     })
+//     console.log(user)
+// }
+
+// main()
+// .catch((e) => console.error(e))
+// .finally(async() => {
+//     await prisma.$disconnect()
+
+// })
---
+++ src/todos/dto/create-todo.dto.ts
@@ -0,0 +1,17 @@
+import { IsNotEmpty, IsString, IsOptional, IsBoolean } from 'class-validator';
+
+export class CreateTodoDto {
+  @IsNotEmpty()
+  @IsString()
+  title: string;
+
+  @IsOptional()
+  @IsString()
+  description: string;
+
+  @IsOptional()
+  @IsBoolean()
+  status: boolean;
+}
+
+
