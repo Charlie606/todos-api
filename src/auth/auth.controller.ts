18c902a9f0ff64ba7c98043f216c3e3473ce3b30
Charlie <patelchandan1212@gmail.com>
2025-04-02 14:21:57 +0545

    auth update

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,17 +3,20 @@ import { AuthService } from './auth.service';
 import { register } from 'module';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
+import { Public } from 'src/helper/public';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  
+  @Public()
   @Post('register')
   register(@Body() registerDto: RegisterDto) {
     return this.authService.register(registerDto);
   }
 
-
+  @Public()
   @Post('login')
   login(@Body() loginDto: LoginDto) {
     return this.authService.login(loginDto);
